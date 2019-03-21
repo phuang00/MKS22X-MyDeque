3c61b0a95ff7f2674256cd4c62cfc3b8106109b9
phuang00 <phuang00@stuy.edu>
2019-03-21 11:40:43 -0400

    wrote constructors and toString and tested it in main

--- MyDeque.java
+++ MyDeque.java
@@ -5,11 +5,11 @@ public class MyDeque<E>{
   private int size, start, end;
 
   public MyDeque(){
-
+    data = (E[])new Object[10];
   }
 
   public MyDeque(int initialCapacity){
-
+    data = (E[])new Object[initialCapacity];
   }
 
   public int size(){
@@ -17,7 +17,21 @@ public class MyDeque<E>{
   }
 
   public String toString(){
-
+    if (start == end) return "{}";
+    String ans = "{";
+    for (int i = start; i <= end || i < size; i++){
+      ans += data[i];
+      if (i != end) ans += " ";
+      else ans += "}";
+    }
+    if (start > end){
+      for (int i = 0; i <= end; i++){
+        ans += data[i];
+        if (i != end) ans += " ";
+        else ans += "}";
+      }
+    }
+    return ans;
   }
 
   public void addFirst(E element){
@@ -29,11 +43,11 @@ public class MyDeque<E>{
   }
 
   public E removeFirst(){
-
-   }
+    return data[0];
+  }
 
   public E removeLast(){
-
+    return data[0];
   }
 
   public E getFirst(){
@@ -42,5 +56,10 @@ public class MyDeque<E>{
 
   public E getLast(){
     return data[end];
+  }
+
+  public static void main(String[] args) {
+    MyDeque<Integer> one = new MyDeque<Integer>(100);
+    System.out.println(one);
   }
 }
