fc9aae3ccabcc7d600eade3deccc024c163680e4
phuang00 <phuang00@stuy.edu>
2019-03-25 11:31:19 -0400

    added Calculator class

---
+++ Calculator.java
@@ -0,0 +1,6 @@
+public class Calculator{
+    /*Evaluate a postfix expression stored in s.
+     *Assume valid postfix notation, separated by spaces.
+     */
+    public static double eval(String s){}
+}
