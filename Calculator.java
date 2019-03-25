9a36abb69bacde10abaff4156f1574575e77a8de
phuang00 <phuang00@stuy.edu>
2019-03-25 11:41:04 -0400

    wrote the part for + operation

--- Calculator.java
+++ Calculator.java
@@ -2,5 +2,35 @@ public class Calculator{
     /*Evaluate a postfix expression stored in s.
      *Assume valid postfix notation, separated by spaces.
      */
-    public static double eval(String s){}
+    public static double eval(String s){
+      MyDeque<Double> nums = new MyDeque<>();
+      String[] tokens = s.split(" ");
+      for (String token: tokens){
+        if (token.equals("+")){
+          double second = nums.removeLast();
+          nums.addLast(nums.removeLast() + second);
+        }
+        else if (token.equals("-")){
+
+        }
+        else if (token.equals("*")){
+
+        }
+        else if (token.equals("/")){
+
+        }
+        else if (token.equals("%")){
+
+        }
+        else{
+          nums.addLast(Double.parseDouble(token));
+        }
+        System.out.println(nums);
+      }
+      return -1;
+    }
+
+    public static void main(String[] args) {
+      eval("2 4 71 8.5 + - * 5 +");
+    }
 }
